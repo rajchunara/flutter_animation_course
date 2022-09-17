6e51be2686014175d66ab9ea068670b5f1e1ed15
rajchunara <rajchunara777@gmail.com>
2022-09-17 14:50:53 -0400

    tap effect

--- lib/animation_screens/first_animation.dart
+++ lib/animation_screens/first_animation.dart
@@ -13,18 +13,37 @@ class _ContainerWithAnimationState extends State<ContainerWithAnimation> {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: AnimatedContainer(
-        curve: Curves.elasticInOut,
-        duration: Duration(milliseconds: 350),
+        onEnd: () {
+          setState(() {
+            sizeFactor = 1;
+          });
+        },
+        curve: Curves.linear,
+        duration: Duration(milliseconds: 80),
         width: 300 * sizeFactor,
         height: 200 * sizeFactor,
         decoration: BoxDecoration(
-            color: Colors.blue[200], borderRadius: BorderRadius.circular(10)),
+          color: Colors.blue[300],
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
       onTap: () {
         setState(() {
-          sizeFactor = (sizeFactor == 1) ? 0.8 : 1;
+          sizeFactor = 0.95;
         });
       },
+      // onTapDown: (s) {
+      //   setState(() {
+      //     // sizeFactor = (sizeFactor == 1) ? 0.8 : 1;
+      //     sizeFactor=0.8;
+      //   });
+      // },
+      // onTapUp: (s) {
+      //   setState(() {
+      //     // sizeFactor = (sizeFactor == 1) ? 0.8 : 1;
+      //     sizeFactor = 1;
+      //   });
+      // },
     );
   }
 }
