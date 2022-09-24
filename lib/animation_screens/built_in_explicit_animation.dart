0769e02d67abbc5ce11399d6d3b40931dff13d86
rajchunara <rajchunara777@gmail.com>
2022-09-24 14:09:21 -0400

    rotation explicit animations

--- lib/animation_screens/built_in_explicit_animation.dart
+++ lib/animation_screens/built_in_explicit_animation.dart
@@ -1,4 +1,5 @@
-
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_course/animation_screens/implicit_animation.dart';
 
 /* 
 
@@ -19,3 +20,40 @@ This above transition widgets extends AnimatedWidget
 
 
  */
+
+class ExplicitBuiltInRotation extends StatefulWidget {
+  ExplicitBuiltInRotation({Key? key}) : super(key: key);
+
+  @override
+  State<ExplicitBuiltInRotation> createState() =>
+      _ExplicitBuiltInRotationState();
+}
+
+class _ExplicitBuiltInRotationState extends State<ExplicitBuiltInRotation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2))
+          ..repeat();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: _animationController,
+      alignment: Alignment.center,
+      child: container,
+    );
+  }
+}
