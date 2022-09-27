1624ed98272968c5acacdc86dd97e5d2e7e4afe3
rajchunara <rajchunara777@gmail.com>
2022-09-26 22:44:31 -0400

    explicit animation using setState

--- lib/animation_screens/explicit_animations.dart
+++ lib/animation_screens/explicit_animations.dart
@@ -12,20 +12,25 @@ class _ExplicitAnimationsState extends State<ExplicitAnimations>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation _colorAnimation;
+  bool darkMode = false;
 
   @override
   void initState() {
     /* In AnimationController we get range of values from 0 to 1 */
     _animationController = AnimationController(vsync: this, duration: duration);
     /* By using tween, we can get range of one color to another and animationController gives control of animation*/
-    _colorAnimation = ColorTween(begin: Colors.amber, end: Colors.purpleAccent)
+    _colorAnimation = ColorTween(begin: Colors.amber, end: Colors.black87)
         .animate(_animationController);
-    _animationController.forward();
+    // _animationController.forward();
 
     /* addListener listens to the change in value when forward() is triggered for animation */
     _animationController.addListener(() {
-      print(_animationController.value);
-      print(_colorAnimation.value);
+      // print(_animationController.value);
+      // print(_colorAnimation.value);
+
+      /* setState to animate(change position or color) on every value change */
+      /* instead of using setState we can also use AnimationBuilder() */
+      setState(() {});
     });
 
     super.initState();
@@ -33,11 +38,21 @@ class _ExplicitAnimationsState extends State<ExplicitAnimations>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: (() {
+        !darkMode
+            ? _animationController.forward()
+            : _animationController.reverse();
+
+        darkMode = !darkMode;
+        print(darkMode);
+      }),
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: _colorAnimation.value),
       ),
     );
   }
