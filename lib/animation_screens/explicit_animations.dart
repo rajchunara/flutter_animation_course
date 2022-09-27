6026b01511a593163386e6c30442cd4312dafcf7
rajchunara <rajchunara777@gmail.com>
2022-09-26 22:33:34 -0400

    some explaination

--- lib/animation_screens/explicit_animations.dart
+++ lib/animation_screens/explicit_animations.dart
@@ -11,15 +11,21 @@ class ExplicitAnimations extends StatefulWidget {
 class _ExplicitAnimationsState extends State<ExplicitAnimations>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+  late Animation _colorAnimation;
 
   @override
   void initState() {
-    // TODO: implement initState
+    /* In AnimationController we get range of values from 0 to 1 */
     _animationController = AnimationController(vsync: this, duration: duration);
+    /* By using tween, we can get range of one color to another and animationController gives control of animation*/
+    _colorAnimation = ColorTween(begin: Colors.amber, end: Colors.purpleAccent)
+        .animate(_animationController);
     _animationController.forward();
 
+    /* addListener listens to the change in value when forward() is triggered for animation */
     _animationController.addListener(() {
       print(_animationController.value);
+      print(_colorAnimation.value);
     });
 
     super.initState();
@@ -27,6 +33,12 @@ class _ExplicitAnimationsState extends State<ExplicitAnimations>
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
   }
 }
