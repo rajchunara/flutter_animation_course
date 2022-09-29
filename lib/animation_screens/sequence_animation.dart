2bc11e216368f8e5cf5097d8530a3f3043e7e2d8
rajchunara <rajchunara777@gmail.com>
2022-09-28 22:25:18 -0400

    sequence animation init

---
+++ lib/animation_screens/sequence_animation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SequenceAnimationOfContainer extends StatefulWidget {
+  SequenceAnimationOfContainer({Key? key}) : super(key: key);
+
+  @override
+  State<SequenceAnimationOfContainer> createState() => _SequenceAnimationOfContainerState();
+}
+
+class _SequenceAnimationOfContainerState extends State<SequenceAnimationOfContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
